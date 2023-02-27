c9418e838998f38d3d2adbf1aed5f9b31eec70a4
tjwrona <tjwrona1992@gmail.com>
2023-02-27 17:49:36 -0500

    Updated handling of paths to fix some broken entries

--- src/common/functions.js
+++ src/common/functions.js
@@ -211,17 +211,210 @@ function validatePassword(password, confirmPassword) {
 
 //Convert a number to a duration "hh:mm:ss"
 function durationString(tSeconds) {
-	var hh = Math.floor(tSeconds / 3600);
-	var mm = Math.floor((tSeconds - (hh * 3600)) / 60);
-	var ss = Math.floor(tSeconds % 60);
+  var hh = Math.floor(tSeconds / 3600);
+  var mm = Math.floor((tSeconds - (hh * 3600)) / 60);
+  var ss = Math.floor(tSeconds % 60);
 
-	if(hh < 10) {hh = "0"+hh;}
-	if(mm < 10) {mm = "0"+mm;}
-	if(ss < 10) {ss = "0"+ss;}
+  if (hh < 10) { hh = "0" + hh; }
+  if (mm < 10) { mm = "0" + mm; }
+  if (ss < 10) { ss = "0" + ss; }
 
-	if(hh > 0) {
-		return hh+":"+mm+":"+ss;
-	} else {
-		return mm+":"+ss;
-	}
+  if (hh > 0) {
+    return hh + ":" + mm + ":" + ss;
+  } else {
+    return mm + ":" + ss;
+  }
+}
+
+function getAvailablePaths(arrangementData) {
+  let pathGroups = {
+    lead: [],
+    altLead: [],
+    bonusLead: [],
+    rhythm: [],
+    altRhythm: [],
+    bonusRhythm: [],
+    bass: [],
+    altBass: [],
+    bonusBass: [],
+  };
+
+  arrangementData.forEach((arrangement) => {
+    const hash = arrangement['arrangementID'];
+    const type = arrangement['type'];
+    const isAlt = arrangement['isAlternateArrangement'];
+    const isBonus = arrangement['isBonusArrangement'];
+
+    if (type === 'Lead') {
+      if (isAlt) {
+        pathGroups.altLead.push({
+          name: 'Alternate ' + type,
+          hash: hash
+        });
+      }
+      else if (isBonus) {
+        pathGroups.bonusLead.push({
+          name: 'Bonus ' + type,
+          hash: hash
+        });
+      }
+      else {
+        pathGroups.lead.push({
+          name: type,
+          hash: hash
+        });
+      }
+    }
+    else if (type === 'Rhythm') {
+      if (isAlt) {
+        pathGroups.altRhythm.push({
+          name: 'Alternate ' + type,
+          hash: hash
+        });
+      }
+      else if (isBonus) {
+        pathGroups.bonusRhythm.push({
+          name: 'Bonus ' + type,
+          hash: hash
+        });
+      }
+      else {
+        pathGroups.rhythm.push({
+          name: type,
+          hash: hash
+        });
+      }
+    }
+    else if (type === 'Bass') {
+      if (isAlt) {
+        pathGroups.altBass.push({
+          name: 'Alternate ' + type,
+          hash: hash
+        });
+      }
+      else if (isBonus) {
+        pathGroups.bonusBass.push({
+          name: 'Bonus ' + type,
+          hash: hash
+        });
+      }
+      else {
+        pathGroups.bass.push({
+          name: type,
+          hash: hash
+        });
+      }
+    }
+  });
+
+  // Update names to include numbers if there is more than one of a path
+  if (pathGroups.lead.length > 1) {
+    let index = 1;
+    pathGroups.lead.forEach((path) => {
+      if (index > 1) {
+        path.name += ' ' + index;
+      }
+      index++;
+    });
+  }
+
+  if (pathGroups.altLead.length > 1) {
+    let index = 1;
+    pathGroups.altLead.forEach((path) => {
+      path.name += ' ' + index;
+      index++;
+    });
+  }
+
+  if (pathGroups.bonusLead.length > 1) {
+    let index = 1;
+    pathGroups.bonusLead.forEach((path) => {
+      path.name += ' ' + index;
+      index++;
+    });
+  }
+
+  if (pathGroups.rhythm.length > 1) {
+    let index = 1;
+    pathGroups.rhythm.forEach((path) => {
+      if (index > 1) {
+        path.name += ' ' + index;
+      }
+      index++;
+    });
+  }
+
+  if (pathGroups.altRhythm.length > 1) {
+    let altRhythmIndex = 1;
+    pathGroups.altRhythm.forEach((path) => {
+      path.name += ' ' + altRhythmIndex;
+      altRhythmIndex++;
+    });
+  }
+
+  if (pathGroups.bonusRhythm.length > 1) {
+    let index = 1;
+    pathGroups.bonusRhythm.forEach((path) => {
+      path.name += ' ' + index;
+      index++;
+    });
+  }
+
+  if (pathGroups.bass.length > 1) {
+    let index = 1;
+    pathGroups.bass.forEach((path) => {
+      if (index > 1) {
+        path.name += ' ' + index;
+      }
+      index++;
+    });
+  }
+
+  if (pathGroups.altBass.length > 1) {
+    let index = 1;
+    pathGroups.altBass.forEach((path) => {
+      path.name += ' ' + index;
+      index++;
+    });
+  }
+
+  if (pathGroups.bonusBass.length > 1) {
+    let index = 1;
+    pathGroups.bonusBass.forEach((path) => {
+      path.name += ' ' + index;
+      index++;
+    });
+  }
+
+  // Add paths to the array in the proper order for display
+  let paths = [];
+  pathGroups.lead.forEach((path) => {
+    paths.push(path);
+  });
+  pathGroups.altLead.forEach((path) => {
+    paths.push(path);
+  });
+  pathGroups.bonusLead.forEach((path) => {
+    paths.push(path);
+  });
+  pathGroups.rhythm.forEach((path) => {
+    paths.push(path);
+  });
+  pathGroups.altRhythm.forEach((path) => {
+    paths.push(path);
+  });
+  pathGroups.bonusRhythm.forEach((path) => {
+    paths.push(path);
+  });
+  pathGroups.bass.forEach((path) => {
+    paths.push(path);
+  });
+  pathGroups.altBass.forEach((path) => {
+    paths.push(path);
+  });
+  pathGroups.bonusBass.forEach((path) => {
+    paths.push(path);
+  });
+
+  return paths;
 }
