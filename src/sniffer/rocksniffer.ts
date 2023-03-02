e56fe826af4d949d8904207638c177f0aa94ee75
tjwrona <tjwrona1992@gmail.com>
2023-03-02 09:27:47 -0500

    no message

---
+++ src/sniffer/rocksniffer.ts
@@ -0,0 +1,13 @@
+class Rocksniffer {
+  // constructor() {
+  //   this.host = sessionStorage.getItem('rocksniffer.host');
+  //   this.port = sessionStorage.getItem('rocksniffer.port');
+  // }
+
+  // sniff() {
+  //   if (!this.connected) {
+  //     //TODO error
+  //     return null;
+  //   }
+  // }
+}
