16e983e777fae06557ae5a8e5d869ffa20f7bc34
tjwrona <tjwrona1992@gmail.com>
2023-06-02 23:26:08 -0400

    Finally actually found a way to remove the cache from the release consistently.

--- rocksniffer_build.js
+++ rocksniffer_build.js
@@ -1,12 +1,17 @@
 const { execSync } = require('child_process');
+const fs = require('fs-extra');
+
+// Remove any old Rocksniffer cache files
+const releasePath = "./RockSniffer/RockSniffer/bin/x64/Release/net6.0-windows";
+if (fs.existsSync(releasePath)) {
+    fs.removeSync(releasePath);
+    console.log(`Removed directory: ${releasePath}`);
+}
 
 // Note: RockSniffer has lots of warnings in the build
 //TODO: Eventually go through all warnings in RockSniffer and resolve them
 try {
     console.log('Executing dotnet build on RockSniffer...');
-    const cleanOutput = execSync('dotnet clean', { cwd: './RockSniffer' });
-    console.log(`RockSniffer Clean Output:\n${cleanOutput}`);
-    
     const buildOutput = execSync('dotnet build /p:Configuration=Release /p:Platform=x64', { cwd: './RockSniffer' });
     console.log(`RockSniffer Build Output:\n${buildOutput}`)
 }
