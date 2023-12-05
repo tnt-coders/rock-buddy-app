15ed040b5b1241a9b282c3d0dedf6224f79b924d
tjwrona <tjwrona1992@gmail.com>
2023-12-04 22:34:36 -0500

    Updated debug script, also updated RockSniffer to handle Badfish by Sublime that had some really bizarre behavior with determining which notes are ignored and which ones aren't.

--- debug/parse_arrangement_xml.pl
+++ debug/parse_arrangement_xml.pl
@@ -47,26 +47,50 @@ for my $i(0 .. $#phrase_iterations) {
     push @phrases, \%phrase_attributes;
 }
 
-# Parse difficulty levels and get all the notes in each
+# Parse difficulty levels and get all the notes and chords in each
 my %notes;
+my %chords;
 for my $level($dom->findnodes('/song/levels/level')) {
     my $difficulty = $level->getAttribute('difficulty');
     $notes{$difficulty} = $level->findnodes('notes/note');
+    $chords{$difficulty} = $level->findnodes('chords/chord');
 }
+
+
+my $total = 0;
 
 # Now loop through phrases and print all the notes in each
 for my $phrase(@phrases) {
     my $difficulty = $phrase->{'difficulty'};
-    my $ignore = $phrase->{'ignore'};
+    #my $ignore = $phrase->{'ignore'};
     my $start_time = $phrase->{'start_time'};
     my $end_time = $phrase->{'end_time'};
 
-    next if $ignore;
+    #next if $ignore;
 
+    my $note_count = 0;
     for my $note(@{$notes{$difficulty}}) {
         my $note_time = $note->getAttribute('time');
         if ($note_time >= $start_time and $note_time < $end_time) {
-            print $note."\n";
+            $note_count++;
+            $total++;
+            #print $note."\n";
         }
     }
+
+    my $chord_count = 0;
+    for my $chord(@{$chords{$difficulty}}) {
+        my $chord_time = $chord->getAttribute('time');
+        if ($chord_time >= $start_time and $chord_time < $end_time) {
+            $chord_count++;
+            $total++;
+            #print $chord."\n";
+        }
+    }
+
+    my $note_and_chord_count = $note_count + $chord_count;
+
+    print "PHRASE - DIFFICULTY ($difficulty) - TIME ($start_time - $end_time) - (NOTES, CHORDS, BOTH): ($note_count, $chord_count, $note_and_chord_count)\n";
 }
+
+print "\nTOTAL: $total\n";
