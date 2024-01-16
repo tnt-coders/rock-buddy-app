61de5046250e878c6a703ae04202a2d1e7edfa86
tjwrona <tjwrona1992@gmail.com>
2024-01-16 12:55:11 -0500

    Updated RockSniffer to provide info about whether Rocksmith is running or not

--- pre_build.py
+++ pre_build.py
@@ -5,16 +5,6 @@ import subprocess
 # Build RockSniffer
 def build_rocksniffer(rocksniffer_srcdir):
 
-    # Remove any old RockSniffer build files
-    rocksniffer_bin = "./RockSniffer/RockSniffer/bin"
-    if os.path.exists(rocksniffer_bin):
-
-        try:
-            shutil.rmtree(rocksniffer_bin)
-            print(f'Removed directory: {rocksniffer_bin}')
-        except Exception as e:
-            print(f'Error: {e}')
-
     # Note: RockSniffer has lots of warnings in the build
     #TODO: Eventually go through all warnings in RockSniffer and resolve them
     print('Executing dotnet build on RockSniffer...')
